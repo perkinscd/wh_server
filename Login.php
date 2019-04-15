0eb78a32e7acbb152798b1e69c8d4c0bfe3e4428
franklinad <franklinda@etsu.edu>
2019-04-15 17:15:50 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- Login.php
+++ Login.php
@@ -1,25 +1,24 @@
 <?php
-	//POST -> email, password
+//POST -> email, password
 
-	define('KEY_LENGTH', 10);
-	require 'DB.php';
+define('KEY_LENGTH', 10);
+require 'DB.php';
 
-	$db = DB::connect();
-	mysqli_set_charset($db, 'utf8');
-	$query = "SELECT * FROM walkhealthy.Login WHERE username = '{$_POST['email']}''";
-	//$query =  mysqli_escape_string($db, $query);
-	$result = mysqli_query($db, $query);
-	if ($result){
-		$result = mysqli_fetch_assoc($result);
-		$salt = $result['passwordSalt'];
-		$hash = $result['passwordHash'];
-		if($hash == hash('sha256', ($_POST['password'] . $salt))){
-			$response = $result['loginId'];
-			$response .= ',' . 'asdflkjh';//random_bytes(KEY_LENGTH); //still can't use random_bytes for some reason
-			echo $response;
-		}else{echo'false';}
-	}else{echo 'false';}
-
+$db = DB::connect();
+mysqli_set_charset($db, 'utf8');
+$query = "SELECT * FROM walkhealthy.Login WHERE username = '{$_POST['email']}''";
+//$query =  mysqli_escape_string($db, $query);
+$result = mysqli_query($db, $query);
+if ($result){
+	$result = mysqli_fetch_assoc($result);
+	$salt = $result['passwordSalt'];
+	$hash = $result['passwordHash'];
+	if($hash == hash('sha256', ($_POST['password'] . $salt))){
+		$response = $result['loginId'];
+		$response .= ',' . 'asdflkjh';//random_bytes(KEY_LENGTH); //still can't use random_bytes for some reason
+		echo $response;
+	}else{echo'false';}
+}else{echo 'false';}
 
 
 ?>
