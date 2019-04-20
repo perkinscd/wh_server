a98e4e05031e4a6d4f1bc13ba3f27cd5ff7536b3
Chelsie <perkinscd@etsu.edu>
2019-04-20 14:58:55 -0400

    Create EventSearch.php
    - Created event search
    - No location used yet

---
+++ EventSearch.php
@@ -0,0 +1,32 @@
+<?php
+    require 'DB.php';
+    header('Content-Type: application/json');
+
+    $db = DB::connect();
+
+    $runningType = $_POST["runningType"];   // Type of running
+    $runningAvailability = $_POST["runningDays"];   // Running Days
+    $runningTime = $_POST["runningTime"];   // Running Times
+
+    $query = "SELECT * FROM walkhealthy.Event WHERE runningType=$runningType AND runningAvailability=$runningAvailability AND runningTime=$runningTime";
+    $result = mysqli_query($db, $query);
+
+    if ($result) {
+        $result_set = array();
+
+        // Fetch multiple rows
+        while ($row = mysqli_fetch_assoc($result)) {
+            // Add to results
+            array_push($result_set, $row);
+        }
+
+        // Send results
+        echo($result_set);
+    } else {
+        echo("false");
+    }
+
+    // Free result set
+    mysqli_free_result($result);
+    mysqli_close($db);
+?>
