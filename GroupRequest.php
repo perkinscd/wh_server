4f0fd796fecae876230768b3c29d43e184ea2fba
Chelsie <perkinscd@etsu.edu>
2019-04-20 15:56:04 -0400

    Create GroupRequest.php
    Creates group requests to be sent to the group owner

---
+++ GroupRequest.php
@@ -0,0 +1,33 @@
+<?php
+    require 'DB.php';
+    header('Content-Type: application/json');
+
+    $db = DB::connect();
+
+    $user = $_POST["userId"];   // Type of running
+    $group = $_POST["groupId"];   // Running Days
+    $owner = null;  // Group owner (retrieved from database)
+    $time = $_POST["timestamp"];    // Time of request initiation from user's system
+    $message = $_POST["message"]; // Message to group owner
+
+    // Get group owner
+    $query = "SELECT userId FROM walkhealthy.GroupOwner WHERE groupId=$group";
+    $result = mysqli_query($db, $query);
+
+    if ($result) {
+        $owner = $result['userId'];
+
+        $newQuery = "INSERT INTO walkhealthy.Request VALUES ($user, $group, $owner, $time, $message)";
+
+        if (mysqli_query($db, $newQuery)) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+    }
+    else {
+        echo("false");
+    }
+
+
+?>
